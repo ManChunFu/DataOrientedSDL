a0cf2367d2ebff1384895380173b2f274384290f
Mandy_Fu <man.chunfu@futuregames.nu>
2020-11-23 12:26:51 +0100

    install sdl2 nuget package

---
+++ DataOrientedSDL/Window.cpp
@@ -0,0 +1,46 @@
+#include "Window.h"
+
+#include <SDL_video.h>
+#include <SDL_render.h>
+#include <iostream>
+
+SDL_Renderer* Engine::Window::Renderer = nullptr;
+
+namespace Engine
+{
+	Window::Window(const std::string& _title, int _width, int _height) : title(_title), width(_width), height(_height){}
+
+	Window::~Window()
+	{
+		if (Renderer)
+		{
+			SDL_DestroyRenderer(Renderer);
+			Renderer = nullptr;
+		}
+
+		if (window)
+		{
+			SDL_DestroyWindow(window);
+			window = nullptr;
+		}
+	}
+
+	bool Window::Init()
+	{
+		window = SDL_CreateWindow(title.c_str(), SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, width, height, 0);
+		if (window == nullptr)
+		{
+			std::cout << "Failed to create window. SDL Error: " << SDL_GetError << std::endl;
+			return false;
+		}
+
+		Renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+		if (Renderer == nullptr)
+		{
+			std::cout << "Failed to create SDL_Renderer. SDL Error: " << SDL_GetError << std::endl;
+			return false;
+		}
+
+		return true;
+	}
+}
---
+++ DataOrientedSDL/Window.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <string>
+#include <SDL.h>
+
+namespace Engine
+{
+	class Window
+	{
+	public:
+		Window(const std::string& _title, int _width, int _height);
+		~Window();
+
+		bool Init();
+		static SDL_Renderer* Renderer;
+		static void RenderClear() { SDL_RenderClear(Renderer); }
+		static void RenderPresent() { SDL_RenderPresent(Renderer); }
+		int ScreenWidth() { return width; }
+		int ScreenHeight() { return height; }
+
+		static SDL_Window* window;
+	private:
+		std::string title;
+		int width;
+		int height;
+
+	};
+}
+
